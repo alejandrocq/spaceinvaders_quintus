a7526dcea6ede9f0b480096d7647e9d4d4bcddb5
Alejandro Castilla <Alejandro Castilla>
2015-10-22 20:47:19 +0200

    Player added

--- scripts/juego.js
+++ scripts/juego.js
@@ -3,14 +3,17 @@ window.addEventListener("load", function () {
   var Q = window.Q = new Quintus({development: true})
     .include("Scenes, Sprites, 2D, Input, Touch, UI, TMX, Audio")
     .setup({
-      width: canvasWidth,   //to fit devices with a screne resolution of 1280 x 720
+      width: canvasWidth,  
       height: canvasHeight,
       scaleToFit: true
     }).controls().touch();
     
     Q.gravityY = 0;
     
+    
+    
     //define player
+    
     Q.Sprite.extend("Player",{
         init: function(p) {
             this._super(p, {
@@ -19,23 +22,27 @@ window.addEventListener("load", function () {
                 y: canvasHeight,
                 speed: 200
         });
+            
+        this.p.y -= this.p.h/2;
         this.add('2d, platformerControls');
 
-      }
+        }
     });
 
     Q.setImageSmoothing(false);
 
     //define scene
     Q.scene("level", function(stage){
-      stage.insert(new Q.Player());
+        Q.stageTMX("level.tmx", stage);
+        //stage.insert(new Q.Player());
+      
     });
 
     //load assets
-    Q.load("sprites.png, sprites.json", function() {    
-    Q.compileSheets("sprites.png","sprites.json");
-    Q.stageScene("level");
-  });
+    Q.loadTMX("level.tmx, sprites.png, sprites.json", function() {   
+        Q.compileSheets("sprites.png","sprites.json");
+        Q.stageScene("level");
+    });
 
 });
         
